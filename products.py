60a41201ed723fcf38330bc0c6906b3d0fb53ee8
BAe <bae0712bar@gmail.com>
2021-04-19 12:38:08 +0800

    isfile

--- products.py
+++ products.py
@@ -1,15 +1,19 @@
+import os #operating system
+
 products = []
+if os.path.isfile('products.csv'):
+	print('檔案存在')
+	with open('products.csv', 'r', encoding = 'utf-8-sig') as f:
+		for line in f:
+			if '商品, 價格' in line:
+				continue #繼續，跳到下一回
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('檔案不存在')
 
-with open('products.csv', 'r', encoding = 'utf-8-sig') as f:
-	for line in f:
-		if '商品, 價格' in line:
-			continue #繼續，跳到下一回
-		name, price = line.strip().split(',')
-		products.append([name, price])
-		#print(name, '的價格是', price, '元')
-print(products)
-
-#讓使用者輸入
+#讓使用者輸入    
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
